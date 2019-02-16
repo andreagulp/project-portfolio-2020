c28309276df34c0e2d16c7c475f21ff7140d7004
Andrea Riccetti <andreariccetti@Andreas-MacBook-Pro.local>
2019-02-16 23:19:41 +0100

    express proxy dev mnot working

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientId:
+    "487400727047-7hr083vknm2o6gkq2v800v50nu6itgc9.apps.googleusercontent.com",
+  googleClientSecret: "lUDzok1tlUQbEw0hC9uqXfLi",
+  mongoUri:
+    "mongodb://andreagulp:andric09856390@ds239055.mlab.com:39055/my-employees-dev",
+  cookieKey: "GHGHGHGHGH6776676776FFFRTTR"
+};
