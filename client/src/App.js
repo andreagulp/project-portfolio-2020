436527473853bebb6a71b13db6e84e750d297b9c
Andrea Riccetti <andreariccetti@Andreas-MacBook-Pro.local>
2019-02-17 17:06:32 +0100

    express password mongo react

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,47 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="/auth/google">
-            Sign in
-          </a>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [activeUser, setActiveUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api/current_user")
+      // .then(response => console.log(response));
+      .then(response => setActiveUser(response.data.name));
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          {!activeUser ? (
+            <a className="App-link" href="/auth/google">
+              Sign in
+            </a>
+          ) : (
+            <a className="App-link" href="/api/logout">
+              Log Out
+            </a>
+          )}
+        </div>
+        <a
+          className="App-link"
+          href="/api/current_user"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Current User - {activeUser}
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default App;
